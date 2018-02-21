53682706bb2a6b570a814646e59bb55d0b107e41
ArthurRosso <arthurderosso@gmail.com>
2018-02-21 15:41:34 -0300

    Adicionar alerts de notificações na página inicial

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-﻿insert into animal (id, nome, data_nasc, peso) values (1, 'José', to_date('06/11/2016', 'dd/mm/yyyy'), 750.0);
+﻿insert into animal (id, nome, data_nasc, peso) values (1, 'José', parseDateTime('20140101','yyyyMMdd'), 750.0);
 
-insert into animal (id, nome, data_nasc, peso) values (2, 'Maria', to_date('15/04/2017', 'dd/mm/yyyy'), 700.0);
+insert into animal (id, nome, data_nasc, peso) values (2, 'Maria', parseDateTime('20140101','yyyyMMdd'), 700.0);
 
-insert into animal (id, nome, data_nasc, peso) values (3, 'João', to_date('12/02/2018', 'dd/mm/yyyy'), 350.0);
+insert into animal (id, nome, data_nasc, peso) values (3, 'João', parseDateTime('20140101','yyyyMMdd'), 350.0);
